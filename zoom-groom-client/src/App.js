25a207180f689cbebe691cd57e295c84a1efb6c1
JaredGold <jegoldstein94@gmail.com>
2021-07-21 14:34:36 +1000

    Add/ Utils folder and Refactored api to single document

--- zoom-groom-client/src/App.js
+++ zoom-groom-client/src/App.js
@@ -1,31 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect } from 'react'
-import axios from 'axios';
+import { useEffect } from "react";
+import api from "./config/api";
 
 function App() {
   useEffect(() => {
-    axios.get("http://localhost:4000/dogs").then(res => console.log(res))
-    }, [])
+    api.get("/dogs").then((res) => console.log(res));
+  }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
