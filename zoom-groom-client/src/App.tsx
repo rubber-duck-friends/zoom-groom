d91359aa570077321a51962cd2f633805f83a69b
JaredGold <jegoldstein94@gmail.com>
2021-07-21 15:44:51 +1000

    Set up root files

--- zoom-groom-client/src/App.tsx
+++ zoom-groom-client/src/App.tsx
@@ -1,12 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Home from "./components/Home";
 import api from "./config/api";
 
 function App() {
+  const [dogs, setDogs] = useState([])
+
   useEffect(() => {
-    api.get("/dogs").then((res) => console.log(res));
+    api.get("/dogs").then((res) => setDogs(res.data));
   }, []);
 
-  return <div className="App"></div>;
+  return(
+    <>
+      <Home dogs={dogs}/>
+    </>
+  )
 }
 
 export default App;
---
+++ zoom-groom-client/src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = (props: any) => {
+  console.log(props.dogs)
+  
+  return(
+    <>
+      <h1>Home Page</h1>
+    </>
+  )
+}
+
+export default Home;
