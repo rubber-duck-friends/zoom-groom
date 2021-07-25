b6c44a3658d421dfc4091b26bd67233d3c1abfeb
kaprestridge <kaprestridge@gmail.com>
2021-07-25 18:59:11 +1000

    added calender to staff page

--- zoom-groom-client/src/components/Staff/Roster.tsx
+++ zoom-groom-client/src/components/Staff/Roster.tsx
@@ -1,9 +1,66 @@
-const Roster = () => {
-  return(
-    <>
-      <h1>Staff Roster Page</h1>
-    </>
-  )
-}
+import * as React from 'react';
+// import { useState } from 'react';
+import Paper from '@material-ui/core/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  DayView,
+  WeekView,
+  MonthView,
+  ViewSwitcher,
+  Toolbar,
+  DateNavigator,
+  TodayButton,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+
+// const currentDate = '2018-11-01';
+const schedulerData = [
+  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
+  { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
+];
+
+
+
+
+const Roster = () => (
+
+  <Paper>
+    <Scheduler
+      data={schedulerData}
+    >
+      <ViewState
+        //TODO
+        //This sets a default start date, it can be controlled to be the current date but I will add this later :)
+        defaultCurrentDate="2021-07-27"
+      />
+      <DayView
+        startDayHour={9}
+        endDayHour={14}
+      />
+      <WeekView
+        startDayHour={10}
+        endDayHour={19}
+      />
+      <WeekView
+        name="work-week"
+        displayName="Work Week"
+        excludedDays={[0, 6]}
+        startDayHour={9}
+        endDayHour={19}
+      />
+      <MonthView />
+      <DayView />
+
+      <Toolbar />
+      <DateNavigator />
+      <TodayButton />
+      <ViewSwitcher />
+      <Appointments />
+    </Scheduler>
+  </Paper>
+
+);
 
 export default Roster
