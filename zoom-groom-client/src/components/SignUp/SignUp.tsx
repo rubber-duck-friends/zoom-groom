af955c86a71aaffc1d761727d9d53f56d894e292
Theosaurus-Rex <theosaurus13@gmail.com>
2021-07-27 08:30:03 +1000

    add JSX for sign up form

--- zoom-groom-client/src/components/SignUp/SignUp.tsx
+++ zoom-groom-client/src/components/SignUp/SignUp.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react"
+// Sends new user details to the backend 
+const signUp = () => {
+
+}
+
 const SignUp = () => {
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [phoneNumber, setPhoneNumber] = useState("")
+  const [password, setPassword] = useState("")
+  const [passwordConfirmation, setPasswordConfirmation] = useState("")
   return(
     <>
       <h1>Sign Up Page</h1>
+      <form onSubmit={signUp}>
+        <label htmlFor="firstName">First Name</label>
+        <input type="text" value={firstName} id="firstName" onChange={(e) => setFirstName(e.target.value)}/>
+        <label htmlFor="lastName">Last Name</label>
+        <input type="text" value={lastName} id="lastName" onChange={(e) => setLastName(e.target.value)}/>
+        <label htmlFor="email">Email</label>
+        <input type="text" value={email} id="email" onChange={(e) => setEmail(e.target.value)}/>
+        <label htmlFor="phoneNumber">Phone Number</label>
+        <input type="text" value={phoneNumber} id="phoneNumber" onChange={(e) => setPhoneNumber(e.target.value)}/>
+        <label htmlFor="password">Password</label>
+        <input type="text" value={password} id="password" onChange={(e) => setPassword(e.target.value)}/>
+        <label htmlFor="passwordConfirmation">Password Confirmation</label>
+        <input type="text" value={passwordConfirmation} id="passwordConfirmation" onChange={(e) => setPasswordConfirmation(e.target.value)}/>
+        <button type="submit">Sign Up</button>
+      </form>
     </>
   )
 }
