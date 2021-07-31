46cd8999b3423c851d2b6fa810fa39f6ba4cd890
JaredGold <jegoldstein94@gmail.com>
2021-07-31 12:02:09 +1000

    feature(zoom-groom-api): add jwt gem to project + add user auth

--- zoom-groom-api/app/controllers/application_controller.rb
+++ zoom-groom-api/app/controllers/application_controller.rb
@@ -1,4 +1,39 @@
 class ApplicationController < ActionController::API
-    # Makes knock and its methods available in all controllers
-    include Knock::Authenticable
+  before_action :authorized
+  
+  def encode_token(payload)
+    JWT.encode(payload, 's3cr3t')
+  end
+  
+  def auth_header
+    # The below will look like this - { Authorization: 'Bearer <token>' }
+    request.headers['Authorization']
+  end
+  
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      # The above will look like this - header: { 'Authorization': 'Bearer <token>' }
+      begin
+        JWT.decode(token, 's3cr3t', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+  
+  def logged_in_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+  
+  def logged_in?
+    !!logged_in_user
+  end
+  
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
 end
--- zoom-groom-api/app/controllers/users_controller.rb
+++ zoom-groom-api/app/controllers/users_controller.rb
@@ -1,28 +1,34 @@
 class UsersController < ApplicationController
-    def create
-        @user = User.create(user_params)
-        if @user.save
-            auth_token = Knock::AuthToken.new payload: {sub: @user.id}
-            render json: {email: @user.email, jwt: auth_token.token}, status: :created
-        else 
-            render json: @user.errors, status: :unprocessable_entity
-        end
+  before_action :authorized, only: [:auto_login]
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: "Invalid username or password"}
     end
+  end
 
-    def sign_in
-        @user = User.find_by_email(params[:email])
+  def sign_in
+    @user = User.find_by(email: params[:email])
 
-        if @user && @user.authenticate(params[:password])
-            auth_token = Knock::AuthToken.new payload: {sub: @user.id}
-            render json: {email: @user.email, jwt: auth_token.token}, status: 200
-        else
-            render json: {error: "Incorrect email or password"}, status: 404
-        end
+    if @user && @user.authenticate(params[:password])
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}, status: 200
+    else
+      render json: {error: "Invalid username or password"}, status: 404
     end
+  end
 
-    private
+  def auto_login
+    render json: @user
+  end
 
-    def user_params
-        params.permit(:first_name, :last_name, :email, :phone_number, :is_staff, :password, :password_confirmation)
-    end
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :phone_number, :is_staff, :password, :password_confirmation)
+  end
 end
