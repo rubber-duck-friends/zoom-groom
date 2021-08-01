4950a8467e2b4d98824dda53d289320b9221ed56
Theosaurus-Rex <theosaurus13@gmail.com>
2021-08-01 13:12:41 +1000

    replace minitest framework with rspec testing framework

--- zoom-groom-api/config/application.rb
+++ zoom-groom-api/config/application.rb
@@ -36,5 +36,6 @@ module ZoomGroomApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators.test_framework = :rspec
   end
 end
