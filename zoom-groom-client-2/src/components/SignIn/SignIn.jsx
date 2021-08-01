e553188851f08edf80d84b6f3b88b139966556b3
JaredGold <jegoldstein94@gmail.com>
2021-08-01 14:04:24 +1000

    feat(zoom-groom-client): add sign in functionality to the client

--- zoom-groom-client-2/src/components/SignIn/SignIn.jsx
+++ zoom-groom-client-2/src/components/SignIn/SignIn.jsx
@@ -1,7 +1,58 @@
+import { useState } from "react"
+import api from "../../config/api"
+
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const submitLogIn = async (event) => {
+    event.preventDefault()
+
+    setErrorMessage("")
+
+    try {
+      const { data } = await api.post("/auth/sign_in", {
+        email: email,
+        password: password
+      })
+      localStorage.setItem("jwt", data.jwt)
+    } catch (error) {
+      setErrorMessage(error.message)
+      setTimeout(() => {
+        setErrorMessage("")
+      }, 2000);
+    }
+  }
+
   return(
     <>
+      {errorMessage && <p>{errorMessage}</p>}
       <h1>Sign In Page</h1>
+      <form onSubmit={submitLogIn}>
+        <label htmlFor="email-input">
+          <input 
+            type="text" 
+            value={email} 
+            id="email-input" onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        
+        <label htmlFor="password-input">
+          <input 
+            type="password"
+            value={password}
+            id="email-input" onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        
+        <label htmlFor="submit-button"/>
+          <input 
+            type="submit" 
+            value="Sign In"  
+          />
+        </label>
+      </form>
     </>
   )
 }
