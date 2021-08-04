639e5464b3f6343cbd308084e7bfda2cb9a7f939
JaredGold <jegoldstein94@gmail.com>
2021-08-04 18:37:58 +1000

    feat(zoom-groom-client): add show appointment page scaffolding

--- zoom-groom-client-2/src/components/Appointments/Appointment.jsx
+++ zoom-groom-client-2/src/components/Appointments/Appointment.jsx
@@ -1,13 +1,65 @@
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+
+const dateString = (date) => {
+  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+};
 
 const Appointment = () => {
-  const { id } = useParams()
-  return(
-    <>
-      <h1>Appointment Page</h1>
-      <p>This page shows appointment id: {id}</p>
-    </>
-  )
-}
+  const [appointmentDate, setAppointmentDate] = useState(new Date());
+  const [pet, setPet] = useState("PLACEHOLDER-PET");
+  const [service, setService] = useState("PLACEHOLDER-SERVICE");
+  const [appointmentTime, setAppointmentTime] = useState({
+    start: "9:00am",
+    end: "9:30am",
+  });
+  const [groomer, setGroomer] = useState("PLACEHOLDER-GROOMER");
+  const { id } = useParams();
 
-export default Appointment
+  const ddmmyyyy = dateString(appointmentDate);
+
+  return (
+    <>
+      <h1>Appointment</h1>
+      <h2>{ddmmyyyy}</h2>
+      <div>
+        <div>
+          <p>
+            Here's the appointment you booked for {pet} for {ddmmyyyy}.<br />
+            See you there!
+          </p>
+        </div>
+        <div>
+          <div>
+            <h4>Pet Name:</h4>
+            <p>{pet}</p>
+          </div>
+          <div>
+            <h4>Date:</h4>
+            <p>{ddmmyyyy}</p>
+          </div>
+          <div>
+            <h4>Service:</h4>
+            <p>{service}</p>
+          </div>
+          <div>
+            <h4>Time:</h4>
+            <p>
+              {appointmentTime.start} - {appointmentTime.end}
+            </p>
+          </div>
+          <div>
+            <h4>Groomer:</h4>
+            <p>{groomer}</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <button>Edit Appointment</button>
+        <button>Cancel Appointment</button>
+      </div>
+    </>
+  );
+};
+
+export default Appointment;
