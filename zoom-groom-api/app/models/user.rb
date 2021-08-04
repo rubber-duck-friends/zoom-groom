6c60d5e4b426215aaddddc32197b11de7dfa08e4
Theosaurus-Rex <theosaurus13@gmail.com>
2021-08-04 15:31:01 +1000

    add logout method that clears jwt from storage and returns to home page

--- zoom-groom-api/app/models/user.rb
+++ zoom-groom-api/app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     validates :email, presence: true, uniqueness: true
     validates :phone_number, presence: true, uniqueness: true
+    has_many :dogs
 end
