315a960d2058f2fe2dcf8105dc3a3ce768951244
JaredGold <jegoldstein94@gmail.com>
2021-08-05 11:38:54 +1000

    feat(zoom-groom-client): add new appointment page

--- zoom-groom-client-2/src/components/Appointments/NewAppointment.jsx
+++ zoom-groom-client-2/src/components/Appointments/NewAppointment.jsx
@@ -1,7 +1,48 @@
+import { useState } from 'react'
+
 const NewAppointment = () => {
+  const [pets, setPets] = useState(["dog1","dog2","dog3"])
+  const [chosenPet, setChosenPet] = useState('')
+  const [availableDates, setAvailableDates] = useState('12pm', '1pm')
+
+  const handleDropdownChange = (callback, event) => {
+    callback(event.target.value)
+  }
+
   return(
     <>
-      <h1>New Appointment Page</h1>
+      <h1>New Appointment</h1>
+      <form>
+        <label htmlFor="pets-dropdown">
+          Pet:
+          <select 
+            id="pets-dropdown"
+            name="pets-dropdown"
+            onChange={handleDropdownChange(setChosenPet)}
+            value={chosenPet}
+          >
+            {pets.map((pet, index) => (
+              <option key={index} value={pet}>{pet}</option>
+            ))}
+          </select>
+        </label>
+        
+        <label htmlFor="date-dropdown">
+          Date:
+          <select 
+            id="date-dropdown"
+            name="date-dropdown"
+            onChange={handleDropdownChange(setChosenPet)}
+            value={chosenPet}
+          >
+            {pets.map((pet, index) => (
+              <option key={index} value={pet}>{pet}</option>
+            ))}
+          </select>
+        </label>
+
+
+      </form>
     </>
   )
 }
