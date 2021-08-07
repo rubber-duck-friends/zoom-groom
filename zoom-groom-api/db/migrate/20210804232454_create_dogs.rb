e463ca4708f63fbd979c919af2375f63e7719196
JaredGold <jegoldstein94@gmail.com>
2021-08-07 10:53:54 +1000

    fix(zoom-groom-api): fixed api

--- zoom-groom-api/db/migrate/20210804232454_create_dogs.rb
+++ zoom-groom-api/db/migrate/20210804232454_create_dogs.rb
@@ -1,12 +1,12 @@
 class CreateDogs < ActiveRecord::Migration[6.1]
   def change
     create_table :dogs do |t|
-      t.string, :name
-      t.DATE, :age
-      t.string, :sex
-      t.boolean, :fixed_status
-      t.string, :breed
-      t.string, :size
+      t.string :name
+      t.datetime :age
+      t.string :sex
+      t.boolean :fixed_status
+      t.string :breed
+      t.string :size
       t.string :groomer_notes
       t.references :user, null: false, foreign_key: true
 
