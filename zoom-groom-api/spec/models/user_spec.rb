867bb0fdec91e1cc61ba15130d3da2b2ad86bdde
kaprestridge <kaprestridge@gmail.com>
2021-08-12 16:30:21 +1000

    added unique email test

--- zoom-groom-api/spec/models/user_spec.rb
+++ zoom-groom-api/spec/models/user_spec.rb
@@ -16,5 +16,9 @@ RSpec.describe User, type: :model do
     user = User.new(id: 1, first_name: "Hello", last_name: "World", phone_number: nil, email: "test@test.com", password_digest: "qi3eroiqhfrou")
     expect(user).to_not be_valid
   end
-
+  it "is not valid if a user with the same email address already exists" do
+    user1 = User.create(id: 1, first_name: "Hello", last_name: "World", phone_number: "8345", email: "test@test.com", password_digest: "qi3eroiqhfrou")
+    user2 = User.new(id: 2, first_name: "Greetings", last_name: "Planet", phone_number: "8345", email: "test@test.com", password_digest: "qi3erowsfkbehfrou")
+    expect(user2).to_not be_valid
+  end
 end
